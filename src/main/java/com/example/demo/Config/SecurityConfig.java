4dec2a24a2a871ed84fc4e9b01bdd0f67837f080
ashish7079 <ashishjha25032004@gmail.com>
2026-09-18 17:00:19 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -67,12 +67,13 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of(
-                        "http://localhost:5173",
-                        "https://exchange-ai-omega.vercel.app"
-                )
-        );
+       configuration.setAllowedOrigins(
+    List.of(
+        "http://localhost:5173",
+        "https://exchange-ai-omega.vercel.app",
+        "https://exchange-ai-d4pi-30r3fbp22-ashish7079s-projects.vercel.app"
+    )
+);
 
         configuration.setAllowedMethods(
                 List.of(
